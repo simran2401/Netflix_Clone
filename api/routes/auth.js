af412c4d0feeaff13596b05de5080d1ff96cc1a6
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-05-28 17:07:33 +0530

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -26,23 +26,30 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(401).json('Wrong password or username!');
 
-    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
-    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
-
-    originalPassword !== req.body.password &&
+    if (!user) {
       res.status(401).json('Wrong password or username!');
+    }
+    else {
+      const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+      const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+      if (originalPassword !== req.body.password) {
+        res.status(401).json('Wrong password or username!');
+      }
+      else {
+        const accessToken = jwt.sign(
+          { id: user._id, isAdmin: user.isAdmin },
+          process.env.SECRET_KEY,
+          { expiresIn: '5d' }
+        );
 
-    const accessToken = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
-      process.env.SECRET_KEY,
-      { expiresIn: '5d' }
-    );
+        const { password, ...info } = user._doc;
 
-    const { password, ...info } = user._doc;
+        res.status(200).json({ ...info, accessToken });
+      }
+    }
 
-    res.status(200).json({ ...info, accessToken });
+
   } catch (err) {
     res.status(500).json(err);
   }
