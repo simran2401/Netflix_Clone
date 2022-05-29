c94b9bd596bd0d3bad0d27bf6b534ba5ba3096f3
Simran Shah <simransah2401@gmail.com>
2022-05-29 22:41:08 +0530

    fifth cimmit

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
+const movieRoute = require('./routes/movies');
+const listRoute = require('./routes/lists');
 
 dotenv.config();
 
@@ -19,6 +21,8 @@ async function main() {
   app.use(express.json());
   app.use('/api/auth', authRoute);
   app.use('/api/users', userRoute);
+  app.use('/api/movies', movieRoute);
+  app.use('/api/lists', listRoute);
 }
 
 app.listen(8800, () => {
